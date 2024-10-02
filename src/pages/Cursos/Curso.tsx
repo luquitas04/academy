60f9642ebced7dc5e7813e33d0d82c261eafc087
Lucas Lopez <lucas.lopez@vistaguay.com>
2024-10-02 12:40:56 -0300

    feat

--- src/pages/Cursos/Curso.tsx
+++ src/pages/Cursos/Curso.tsx
@@ -4,14 +4,15 @@ import styles from './Cursos.module.scss';
 const Curso = () => {
     return (
         <section className={styles.container} id='contacto'>
+            <div className={styles.figura}>
+                <img src="/assets/img/macbook.png" />
+            </div>
             <div className={styles.text}>
                 <h3>Curso completo de CSS3. ¡Domina CSS de una vez por todas!</h3>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, aliquam nemo in facere suscipit at delectus unde labore ad officia iste accusantium eaque aperiam laborum est voluptate totam? Modi, obcaecati?</p>
                 <Button text='Entre al curso' className={styles.btn} />
             </div>
-            <div className={styles.figura}>
-                <img src="/assets/img/macbook.png" />
-            </div>
+            
         </section>
     )
 }
